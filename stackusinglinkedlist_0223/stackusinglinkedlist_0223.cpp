55800c61afab28ba08859c195f8198a3c617d869
Irfansyah001 <syahirfan545@gmail.com>
2024-05-11 11:31:17 +0700

    add else on void peek

--- stackusinglinkedlist_0223/stackusinglinkedlist_0223.cpp
+++ stackusinglinkedlist_0223/stackusinglinkedlist_0223.cpp
@@ -41,3 +41,10 @@ public:
 		if (top == NULL) {
 			cout << "List is empty." << endl;
 		}
+		else {
+			Node* current = top;
+			while (current != NULL) {
+				cout << current->data << "   " << endl;
+				current = current->next;
+			}
+			cout << endl;
